63983a517ea028cb5522cebc6e3ab09013a28f09
Mohammad Taghi Gholami <info@designkadeh.com>
2018-11-16 01:31:07 +0330

    Delete footer.php

--- footer.php
+++
@@ -1,5 +0,0 @@
-<script src="<?php echo $db->internalUrl("/view") ?>/assets/js/jquery.min.js"></script>
-<script src="<?php echo $db->internalUrl("/view") ?>/assets/js/bootstrap.min.js"></script>
-<script src="<?php echo $db->internalUrl("/view") ?>/assets/js/custom.js"></script>
-</body>
-</html>
